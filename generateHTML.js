70550479d5f8033dd9002fe2181e7b60b8c136fd
Jake Walker <jcw2865@gmail.com>
2020-01-21 22:38:20 -0600

    Restructured inquirer prompts to have conditional statements. Cards for individual employees now displaying on HTML with their relative employee-type information.

--- generateHTML.js
+++ generateHTML.js
@@ -1,22 +1,110 @@
 // Data for each employee role (not sure about this section yet)
-const employeeRole = {
-    manager: {
-        name: "",
-        email: "",
-    },
-    engineer: {
-        name: "",
-        github: "",
-    },
-    intern: {
-        name: "",
-        school: "",
-    }
-};
+// const employeeRole = {
+//     manager: {
+//         name: "",
+//         email: "",
+//     },
+//     engineer: {
+//         name: "",
+//         github: "",
+//     },
+//     intern: {
+//         name: "",
+//         school: "",
+//     }
+// };
+
+// const managerCard = require("./manager.html");
 
 
-function generateHTML(data) {
+function generateHTML(managerResponse, employeeArray) {
     return `
-
+    <!DOCTYPE html>
+    <html lang="en">
+    
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Team Profile Generator</title>
+        <style>
+            body {
+                background-image: url(./Photos/Cool-Background.jpeg)
+            }
+    
+            .header {
+                padding: 60px;
+                text-align: center;
+                background: #1abc9c;
+                color: white;
+                font-size: 30px;
+            }
+    
+            .cardContainer {
+                display: flex;
+                justify-content: space-evenly;
+                flex-wrap: wrap;
+            }
+    
+            .card {
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+                transition: 0.3s;
+                width: 40%;
+                float: left;
+                margin: 10px
+            }
+    
+            .card:hover {
+                box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.2);
+            }
+    
+            .container {
+                padding: 2px 16px;
+                background-image: url(./Photos/yellow-notebook.jpg);
+            }
+    
+            h4 {
+                background-color: lightpink;
+                text-align: center;
+                padding-top: 10px;
+                padding-bottom: 10px;
+            }
+        </style>
+    </head>
+    
+    <body>
+        <div class="header">
+            <h1>The Dream Team</h1>
+        </div>
+        <div class="cardContainer">
+            <div class="card">
+                <div class="container">
+                    <h4>
+                        <b>${managerResponse.manager}</b>
+                        <p>Manager</p>
+                    </h4>
+                    <p><strong>ID: </strong>${managerResponse.managerId}</p>
+                    <p><strong>Email: </strong>${managerResponse.managerEmail}</p>
+                    <p><strong>Phone: </strong>${managerResponse.managerPhone}</p>
+                </div>
+            </div>
+            ${employeeArray.map(employee => `
+            <div class="card">
+            <div class="container">
+                <h4>
+                    <b>${employee.getName()}</b>
+                    <p>${employee.getRole()}</p>
+                </h4>
+                <p><strong>ID: </strong>${employee.getId()}</p>
+                ${employee.getGhusername() ? `<p><strong>GitHub Username: ${employee.getGhusername()}</strong></p>` : ''}
+                ${employee.getSchool() ? `<p><strong>School: ${employee.getSchool()}</strong></p>` : ''}
+            </div>
+        </div>
+            `)}
+        </div>
+    </body>
+    
+    </html>
 `
 }
+module.exports = generateHTML;
--- index.js
+++ index.js
@@ -1,39 +1,100 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateHTML = require("./generateHTML");
+const Employee = require("./lib/employee");
+const managerQuestions = [
 
-function promptUser() {
-    inquirer
-        .prompt([
-            // #####
-            // QUESTIONS FOR MANAGER
-            // #####
-            {
-                type: "input",
-                name: "manager",
-                message: "Welcome, manager. Please enter your name."
-            },
-            {
-                type: "input",
-                name: "managerEmail",
-                message: "Please enter your email."
-            },
-            {
-                type: "number",
-                name: "teamSize",
-                message: "Please enter the number of employees you have. (Numbers only)"
-            },
-            // #####
-            // QUESTIONS FOR EMPLOYEES
-            // #####
-            {
-                type: "list",
-                name: "role",
-                message: "What is your role in the organization?",
-                choices: [
-                    "Manager",
-                    "Engineer",
-                    "Intern"
-                ]
-            }
-        ]);
-}
+    // #####
+    // QUESTIONS FOR MANAGER
+    // #####
+    {
+        type: "input",
+        name: "manager",
+        message: "Welcome, manager. Please enter your name."
+    },
+    {
+        type: "input",
+        name: "managerEmail",
+        message: "Please enter your email."
+    },
+    {
+        type: "input",
+        name: "managerId",
+        message: "Please enter your ID."
+    },
+    {
+        type: "input",
+        name: "managerPhone",
+        message: "Please enter your phone number."
+    },
+    {
+        type: "number",
+        name: "teamSize",
+        message: "Please enter the number of employees you have. (Numbers only)"
+    }
+];
+const employeeQuestions = [
+    // #####
+    // QUESTIONS FOR EMPLOYEES
+    // #####
+    {
+        type: "input",
+        name: "employeeName",
+        message: "Please enter an employee name."
+    },
+    {
+        type: "input",
+        name: "employeeId",
+        message: "Please enter their ID."
+    },
+    {
+        type: "list",
+        name: "employeeRole",
+        message: "Please enter their role in the organization.",
+        choices: [
+            "Engineer",
+            "Intern"
+        ]
+    }
+]
+const engineerUsername = [
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter their GitHub username."
+    }
+]
+const internSchool = [
+    {
+        type: "input",
+        name: "school",
+        message: "Please enter their school name."
+    }
+]
+
+async function init() {
+    const managerResponse = await inquirer.prompt(managerQuestions);
+    questionNum = managerResponse.teamSize;
+    const employeeArray = [];
+    for (i = 0; i < questionNum; i++) {
+        const employeeResponse = await inquirer.prompt(employeeQuestions);
+        if (employeeResponse.employeeRole === "Engineer") {
+            // Ask for GH username
+            const engineerResponse = await inquirer.prompt(engineerUsername);
+            const thisEmployee = new Employee(employeeResponse.employeeName, employeeResponse.employeeId, employeeResponse.employeeRole, engineerResponse.username, null);
+            employeeArray.push(thisEmployee);
+        } else {
+            // Ask for school name
+            const internResponse = await inquirer.prompt(internSchool);
+            const thisEmployee = new Employee(employeeResponse.employeeName, employeeResponse.employeeId, employeeResponse.employeeRole, null, internResponse.school);
+            employeeArray.push(thisEmployee);
+        }
+    }
+    // console.log(employeeArray);
+    const fileHTML = await generateHTML(managerResponse, employeeArray);
+    fs.writeFile("main.html", fileHTML, function (err) {
+        // console.log(err);
+    })
+
+};
+init();
